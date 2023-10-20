1bd354888df0014238822dd8ca8e605be0abc10a
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-10-20 15:55:07 +0700

    Tokopedia.com (Scrape): I start scraping data from Tokopedia web since indeed cant be scrape

--- main-indeed.py
+++ main-indeed.py
@@ -1,7 +1,11 @@
+import httpx
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+"""It seems that indeed.com can't be scraped anymore and needs a custom API or they have a new policy about their 
+API, so I'll forget about it for now"""
 
 # Selenium
 driver = webdriver.Chrome()
@@ -18,11 +22,25 @@ params = {
 }
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, '
                          'like Gecko) Chrome/118.0.0.0 Mobile Safari/537.36'}
-res = requests.get(url, params=params, headers=headers)
+res = requests.get(url)
+print(res.status_code)
 
 # BeautifulSoup
 soup = BeautifulSoup(page_source, 'html.parser')
-soup = soup.find('nav', 'css-jbuxu0 ecydgvn0')
+soup = soup.find('div', 'mosaic-jobResults')
 pages = soup.find_all('a')
-for page in pages:
-    print(page.text)
+print(soup.prettify())
+
+def get_item():
+    bsoup = BeautifulSoup(page_source, 'html.parser')
+    bsoup = bsoup.find('div', 'mosaic-zone')
+    items = bsoup.find_all('a')
+    for item in items:
+        print(item.text)
+
+
+if __name__ == '__main__':
+    try:
+        get_item()
+    except:
+        pass
---
+++ main-tokopedia.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+
+"""
+Scraping Data from Tokopedia.com
+"""
+
+url = 'https://www.tokopedia.com/search?st=product&q=bangku%20gaming'
+params = {
+    'st': 'product',
+    'q': 'bangku gaming',
+}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/118.0.0.0 Safari/537.36',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Accept-Language': 'en-US,en;q=0.9',
+           'Referer': 'https://www.tokopedia.com/search?',
+           'Upgrade-Insecure-Requests': '1',
+           'TE': 'Trailers',}
+
+respons = requests.get(url, headers=headers)
+
+if respons.status_code == 200:
+    print(respons.status_code)
+else:
+    print("Request Failed : ", respons.status_code)
+
+
+# BeautifulSoup
+def get_items():
+    soup = BeautifulSoup(respons.content, 'html.parser')
+    items = soup.find_all('div', 'css-1asz3by')
+    i = 0
+    for item in items:
+        name = item.find('div', 'prd_link-product-name')
+        print(name.text)
+
+
+def get_price():
+    soup = BeautifulSoup(respons.content, 'html.parser')
+    items = soup.find_all('div', 'css-1asz3by')
+    i = 0
+    for item in items:
+        price = item.find('div', 'prd_link-product-price')
+        print(price.text)
+
+
+if __name__ == '__main__':
+    get_items()
+    get_price()
+
