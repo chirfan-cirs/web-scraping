2fda51911a91c4382c7b9bd7df5328016d0daeda
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-10-26 14:46:21 +0700

    Detik.com (Scrape): Scrape Data title and link from Detik.com

---
+++ main-detik.py
@@ -0,0 +1,26 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+url = requests.get('https://www.detik.com/terpopuler')
+
+# Beautifulsoup
+bs = BeautifulSoup(url.text, 'html.parser')
+result = bs.find('div', 'list-content')
+
+
+def get_links():
+    links = result.findAll('div', 'media__text')
+    for link in links:
+        print(link.find('a').get('href'))
+
+
+def get_item():
+    titles = result.findAll('div', 'media__text')
+    for title in titles:
+        print(title.find('a').text)
+
+
+if __name__ == '__main__':
+    get_item()
+    get_links()
