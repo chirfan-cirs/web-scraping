8072cd454193a623763537e1148fd8fd99577dea
chirfan1 <44518884+chirfan1@users.noreply.github.com>
2023-10-31 15:24:48 +0700

    kompas.com (Scrape): Scrape Data titles and links from kompas.com

--- main-kompas.py
+++ main-kompas.py
@@ -8,9 +8,16 @@ if url.status_code == 200:
 
     result = soup.find('div', {'class': 'mostWrap'})
     titles = result.find_all('h2', 'mostTitle')
+    links = result.find_all('div', 'mostItem')
 
     for title in titles:
         print(title.text.strip())
+    for link in links:
+        try:  # Using Try Except to get the link, because not all of them have the 'a' tag and for
+            # anticipation if there is a missing some tag ('a' for example)
+            print(link.find('a').get('href'))
+        except Exception:
+            pass
 
 else:
     print("Can't Scrape")
